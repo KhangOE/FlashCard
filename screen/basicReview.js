4c6cba0e232fc11e426169a360747f7cdf2ef966
Viet Tien <114817514+llewisd@users.noreply.github.com>
2023-01-24 16:42:09 +0700

    Update basicReview.js

--- screen/basicReview.js
+++ screen/basicReview.js
@@ -1,12 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, StyleSheet, ScrollView, Pressable, TouchableHighlight, TouchableOpacity, Animated, Easing, Dimensions, SafeAreaView } from 'react-native';
 import { FontAwesome, AntDesign, Entypo, Feather, SimpleLineIcons } from '@expo/vector-icons';
 import axios from 'axios';
 import { checkDoc } from '../api/firebaseApi';
 import { Audio } from 'expo-av';
 
+const tenvhHeight = Dimensions.get('screen').height * 0.1;
+const cardWidth = Dimensions.get('window').width * 0.8;
+
+function CardReview() {
+  const rotate = useRef(new Animated.Value(0)).current;
+  const [isFlipped, setIsFlipped] = useState(false);
+  const rotateFront = rotate.interpolate({
+    inputRange: [0,1],
+    outputRange: ['0deg','180deg']
+  });
+  const rotateBack = rotate.interpolate({
+    inputRange: [0,1],
+    outputRange: ['180deg','360deg']
+  });
+  
+  function flipCard() {
+      Animated.timing(rotate, {
+        toValue: isFlipped ? 0 : 1,
+        duration: 400,
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start(() => setIsFlipped(!isFlipped));
+
+  }
+  return (
+    <Pressable style={styles.card} onPress={flipCard} android_disableSound={true}>
+      <View style={styles.cardWrapper}>
+          <Animated.View style={[styles.cardFront, {transform: [{rotateY: rotateFront}]} ]}>
+              <Text style={styles.vocabulary}> green </Text>
+              <View style={styles.sound}>
+                <AntDesign name="sound" size={24} color="black" /> 
+              </View>  
+          </Animated.View>
+          <Animated.View style={[styles.cardBack, {transform: [{rotateY: rotateBack}]} ]}>
+              <Text style={styles.cardBackText}>màu xanh </Text> 
+          </Animated.View>
+      </View>
+    </Pressable>
+  );
+}
+
 
 function BasicReviewScreen({ navigation, route }) {
+    // Xu ly am thanh
     const [card, setCard] = useState([])
     const [sound, setSound] = useState();
     useEffect(() => {
@@ -40,18 +82,80 @@ function BasicReviewScreen({ navigation, route }) {
             }
             : undefined;
     }, [sound]);
+    
+    // Xử lý animation flip va scroll
+  const cardTotal = 12;     // Tong so luong card
+  const scrollPoint = useRef(0);
+  let distance = cardWidth + 20;
+  const [cardNumber, setCardNumber] = useState(1);
+  function moveLeftRight(index) {
+    if (index == 1) {
+      scrollPoint.current.scrollTo({x: distance * cardNumber, animated: true});
+      setCardNumber(cardNumber == cardTotal ? cardTotal : cardNumber + 1);
+    }
+    else {
+      scrollPoint.current.scrollTo({x: distance * (cardNumber - 2), animated: true});
+      setCardNumber(cardNumber == 1 ? 1 : cardNumber - 1);
+    }
+    
+  }
+  // Trang chia thành 3 thành phần chính : navbar, scrollview , footer
     return (
-        <View style={styles.base}>
-            {card?.map(item => {
-                return <>
-                    <View>
-                        <Text>
-                            {item?.vi}
-                        </Text>
-                    </View>
-                </>
-            })}
+       <SafeAreaView style={styles.base}>
+        <View style={styles.navbar}>
+          <View style={styles.sub_block}>
+            <View style={styles.heading}>
+              <TouchableHighlight>
+                <AntDesign name="arrowleft" size={24} color="white" />
+              </TouchableHighlight>
+              <Text style={styles.topicTitle}> Color </Text>
+            </View>
+            <TouchableHighlight style={{opacity: 0}}>
+              <FontAwesome name="search" size={20} color="white" />
+            </TouchableHighlight>
+          </View>
         </View>
+
+        <ScrollView style={styles.mainSection} horizontal={true} showsHorizontalScrollIndicator={false}
+        scrollEnabled={false}
+        decelerationRate="fast"
+        snapToInterval={cardWidth + 22}
+        snapToAlignment={"center"}
+        contentInset={{
+          top: 0,
+          left: 10,
+          bottom: 0,
+          right: 10,
+        }}
+        ref={scrollPoint}
+        >
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+          <CardReview />
+        </ScrollView>
+        
+
+        <View style={styles.footerCard}>
+          <View style={styles.footerCardBlock}>
+            <TouchableOpacity onPress={() => moveLeftRight(-1)}>
+              <Feather name="arrow-left-circle" size={40} color="#6A197D" />
+            </TouchableOpacity>
+            <Text style={styles.cardPosition}> {cardNumber} / {cardTotal} </Text>
+            <TouchableOpacity onPress={() => moveLeftRight(1)}>
+              <Feather name="arrow-right-circle" size={40} color="#6A197D" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     );
 }
 
